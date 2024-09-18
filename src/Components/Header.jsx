a44e8a19b69ae184fd745c2168f4edfb99d18b64
Ahmed <jacfarmahamed77@gmail.com>
2024-09-18 20:20:45 +0300

    title change

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,6 @@
 function Header(){
     return <div className="bg-blue-400 px-6 py-2">
-        <h1 className="text-4xl font-semibold">Somali <span className="text-yellow-400">Quotes</span></h1>
+        <h1 className="text-4xl font-semibold">Somali <span className="text-yellow-400">Maah maah</span></h1>
     </div>
 }
 
